fc10976eaf24c55690e9f98ec094bddfc63679e1
Javier Zapata <javierzapata82@gmail.com>
2020-06-14 17:11:10 +0200

    Setup "routing"

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,27 @@
 <script>
+	import { view } from './stores.js';
+
+  import ActivityLog from './pages/ActivityLog.svelte';
+  import Currency from './pages/Currency.svelte';
+  import Dashboard from './pages/Dashboard.svelte';
+  import Settings from './pages/Settings.svelte';
+
+	let currentView;
+
+  const views = {
+    'activity-log': ActivityLog,
+    'currency': Currency,
+    'dashboard': Dashboard,
+    'settings': Settings,
+  };
+
+	const unsubscribe = view.subscribe(value => {
+		currentView = value;
+	});
 </script>
 
 <main>
+  <svelte:component this={views[currentView]}/>
 </main>
 
 <style>
